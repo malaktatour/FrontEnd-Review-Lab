1427c4328bf779f59cb22ed333ce04defd7cffd0
malaktatour <109721832+malaktatour@users.noreply.github.com>
2022-07-25 18:08:05 +0300

    finishing the whole lab

---
+++ pythonReview.py
@@ -0,0 +1,22 @@
+def create_youtube_video (title , descrption):
+	title = input ("enter the video you want to watch")
+	descrption = input ("descrption")
+	youtube_video = {"title" : title , "descrption" :  descrption , "likes " : "number of likes", "dislikes" : "number of dislikes" , "comments" : ""}
+	comments = {"username": ""}
+	return youtube_video
+
+def like ():
+	youtube_video= input ("")
+		if “likes” in youtube_video:
+    		return like += 1
+
+
+def dislike ():
+	if "dislikes" in youtube_video :
+		return like += 1
+
+def add_comment (youtube_video ,  username , comment_text ):
+	youtube_video ["username"] = "comment_text"
+	return youtube_video
+	
+
---
+++ session1.py
@@ -0,0 +1,3 @@
+def create_youtube_video(title , descrption):
+	youtube_video = {"title" : "the name of the video" , "descrption" : "the descrption","like" :"numbers of likes" ,"dislikes" : "the number of dislikes" , "comments" : "" }
+
